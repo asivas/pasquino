8a6e08ff281cc62c92929f156dbdbd646c55215d
vidaguren <vidaguren>
2006-11-02 21:35:29 +0000

    Se agrega dateTimeFmt

---
+++ visual/dateTimeFmt.class.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* Se define la clase dateTimeFmt 
+*
+* @author       Lucas Vidaguren <vidaguren@econ.unicen.edu.ar>
+* @copyright    Lucas Vidaguren <vidaguren@econ.unicen.edu.ar>
+*
+* @package      visual
+* @since 1.0 - 02/11/2006
+*/
+
+/**
+* Tiene funciones de traducción entre formato e internación de fechas y tiemo
+*
+* @author       Lucas Vidaguren <vidaguren@econ.unicen.edu.ar>
+* @copyright    Lucas Vidaguren <vidaguren@econ.unicen.edu.ar>
+*
+* @package      visual
+* @since 1.0 - 02/11/2006
+*/
+class dateTimeFmt {
+
+    function dateTimeFmt() {
+    }
+    
+    function segundosAStrTiempo($segundos)
+    {
+        $horas = $segundos>0?floor($segundos/3600):ceil($segundos/3600);
+        $minutos = abs(ceil($segundos/60) - $horas*60);
+        
+        return sprintf("%02d:%02d",$horas,$minutos);
+    }
+    
+    /**
+     * Convierte una fecha en el formato
+     */
+    function fechaArgtotime($fecha)
+    {
+        if(ereg ("([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})", $fecha, $dmY))
+           return strtotime("{$dmY[3]}-{$dmY[2]}-{$dmY[1]}");
+        
+        if(!empty($fecha))
+          return strtotime($fecha);
+        
+        return time();
+    }
+}
