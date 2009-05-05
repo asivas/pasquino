57047cac627502e8f284e60e8ff5b5b51551ffb3
vidaguren <vidaguren>
2009-05-05 19:10:20 +0000

    Usando criterio

--- SistemaFCE/dao/DaoBase.class.php
+++ SistemaFCE/dao/DaoBase.class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('datos/criterio/Criterio.class.php');
+
 abstract class DaoBase {
     /**
      * var object $_db 
@@ -22,27 +24,47 @@ abstract class DaoBase {
      */
     protected $tableNale;
     
-    
+    /**
+     * Constructor de DaoBase
+     */
     function DaoBase() {
         $this->_db = &ADONewConnection(Configuracion::dbms); # eg 'mysql' or 'postgres'
         $this->_db->SetFetchMode(ADODB_FETCH_ASSOC);
         $this->_db->Connect(Configuracion::dbHost, Configuracion::dbUser, Configuracion::dbPassword, Configuracion::dbName);
     }
     
+    /**
+     * Crea el arreglo buffer para guardar
+     * @return array arreglo con datos con nombre de la columna de la base (nombreCol => valor)
+     */
     protected abstract function _buffer($elem){}
+    
+    /**
+     * Crea el objeto de la entidad a la cual logra el acceso el DAO
+     * @return object el objeto con los datos a partir de $row
+     * @param array $row arreglo con los datos obtenidos de la base en forma nombreCol => valor
+     */
     protected abstract function crearObjetoEntidad($row) {}
     
-    
+    /**
+     * Obtiene la referencia a la base de datos
+     * @return object Referencia al objeto ADODb
+     */
     function getDb()
     {
     	return $this->_db;
     }
     
-    function findBy($filtros = null,$order=null){
+    /**
+     * Obtiene una lista de objetos de la entidad
+     * @param object $filtro Objeto de clase Criterio
+     * @param string $order Columna o columnas separadas por coma (,) para ordenar la busqueda 
+     */
+    function findBy($filtro = null,$order=null){
         
         $sql = $this->baseFindBySQL;
         
-        $sql .= $this->buildCond($filtros);
+        $sql .= $filtro->getCondicion();
         
         if(!isset($order)) $order = $this->defaultOrder;
             
@@ -57,26 +79,6 @@ abstract class DaoBase {
             $lista[] = $this->crearObjetoEntidad($row);
         }
         return $lista;
-    }
-    
-    function buildCond($filtros)
-    {
-    	$cond = "";
-        if(isset($filtros) && is_array($filtros))
-        {
-            foreach($filtros as $field => $val)
-            {
-                if($field{0}!='_')
-                {
-                    if(empty($cond))
-                        $cond = " WHERE ";
-                    else
-                        $cond .= " AND ";
-                    $cond .= "{$field} = '{$val}'";    
-                }
-            }
-        }
-        return $cond;
     }
     
     function save($elem) {
