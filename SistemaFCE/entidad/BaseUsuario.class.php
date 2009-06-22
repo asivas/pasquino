1ed151439ab7bccbe49e51ad59f33dfc8b63c741
vidaguren <vidaguren>
2009-06-22 23:13:13 +0000

    creacion de baseUsuario y baseUsuarioMod que manejan una tabla con permisos de usuario

---
+++ SistemaFCE/entidad/BaseUsuario.class.php
@@ -0,0 +1,70 @@
+<?php
+
+class BaseUsuario {
+    
+    private $id;
+    private $permisos;
+    private $nombre;
+    private $apellido;
+    
+    function __construct() {
+    }
+    
+    function getId()
+    {
+        return $this->id;	
+    }
+    
+    function setId($id)
+    {
+    	$this->id = $id;
+    }
+    
+    function getPermisos()
+    {
+    	return $this->permisos;
+    }
+    
+    function setPermisos($permisos)
+    {
+    	$this->permisos = $permisos;
+    }
+    
+    function getNombre()
+    {
+        return $this->nombre;   
+    }
+    
+    function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    
+    function getApellido()
+    {
+        return $this->apellido;   
+    }
+    
+    function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+    
+    function agregarPermiso($permiso)
+    {
+    	if(!$this->tienePermiso($permiso))
+            $this->permisos .= "\n$permiso";
+    }
+    
+    function quitarPermiso($permiso)
+    {
+    	$this->permisos = str_replace("\r\n$permiso","",$this->permisos);
+        if($this->tienePermiso($permiso))
+            $this->permisos = str_replace("$permiso","",$this->permisos); 
+    }
+    
+    function tienePermiso($permiso)
+    {
+        return strpos($this->permisos,$permiso)!==FALSE;
+    }
+}
---
+++ SistemaFCE/modulo/BaseUsuarioMod.class.php
@@ -0,0 +1,129 @@
+<?php
+
+require_once('SistemaFCE/modulo/BaseMod.class.php');
+
+class BaseUsuarioMod extends BaseMod{
+    
+    protected $_daoU;
+    
+    function __construct($skinDirName=null)
+    {
+    	$this->_daoU =  new DaoUsuario();
+        parent::__construct($skinDirName);
+    }
+    
+    function lista()
+    {
+        $usuarios = $this->_daoU->findBy();
+        
+        $this->smarty->assign('permisos',$this->_getPermisosVigentes());
+        $this->smarty->assign('usuarios',$usuarios);
+        
+        $this->mostrar('usuario/listar.tpl');
+    }  
+    
+    function accionImportar($req)
+    {
+        $usr = new Usuario();
+        $usr->setId($req['id_usuario']);
+        $usr->setPermisos("");
+        $this->_daoU->save($usr);
+        
+        //una vez creado lo vuelvo a leer
+        $this->form($req);
+    }  
+    
+    function formImportar()
+    {
+        $usuarios = $this->_daoU->findPosiblesUsuarios();
+        $this->smarty->assign('usuarios',$usuarios); 
+        $this->mostrar('usuario/importar.tpl');
+    }
+    
+    function form($req=null)
+    {
+    	if(!isset($req)) //form de alta
+        {
+        	$this->formImportar();
+            return;
+        }
+        
+        $usuario = $this->_daoU->findById($req['id_usuario']);
+        
+        $this->smarty->assign('permisos',$this->_getPermisosVigentes());
+        $this->smarty->assign('usuario',$usuario);
+        
+        $this->mostrar('usuario/formulario.tpl');
+    }
+    
+    function baja($req)
+    {
+    	$this->_daoU->deletePorId($req['id_usuario']);
+    }
+    
+    function modificacion($req)
+    {
+        $usuario = $this->_daoU->findById($req['id_usuario']);
+        
+        $permisosPosibles = $this->_getPermisosVigentes();
+        
+        foreach($permisosPosibles as $perm)
+        {
+        	if(isset($req[$perm]))
+                $usuario->agregarPermiso($perm);
+            else
+                $usuario->quitarPermiso($perm);
+        }
+        
+        $this->_daoU->save($usuario);
+    }
+    
+    protected function _getArrPermisos(&$permisos,$permisosXml)
+    {
+    	if(empty($permisosXml)) return;
+        
+        if(empty($permisosXml->permiso)) return;
+        
+        foreach($permisosXml->permiso as $perm)
+        {
+            //lo guardo en el key y en el valor para que sea unico
+            $permisos[(string)$perm] = (string)$perm;   
+        }
+        
+    }
+    
+    protected function _getPermisosVigentes()
+    {
+    	$permisos = array();
+        $modsConf = Configuracion::getModulosConfig();
+        //recorro los modulos
+        foreach($modsConf->modulo as $mod)
+        {   
+            $n = (string)$mod['nombre'];
+            //busco los permisos de las acciones
+            $accionesMod = $mod->acciones;
+            if(!empty($accionesMod->accion))
+            {
+            	foreach($accionesMod->accion as $acc)
+                {
+                	$this->_getArrPermisos($permisos,$acc->permisos);
+                }
+            }
+            
+            //busco los permisos de los menues
+            $menuMod = $mod->menuPrincipal;
+            if(!empty($menuMod->menuItem))
+            {
+                foreach($menuMod->menuItem as $item)
+                {
+                    $this->_getArrPermisos($permisos,$item->permisos);
+                }
+            }
+            
+            //y los del menú principal
+            $this->_getArrPermisos($permisos,$menuMod->permisos);
+        }
+        return $permisos;
+
+    }
+}
