a38248b21b19d242492b1472855b76a05f70037a
martinezdiaz <martinezdiaz>
2010-04-13 15:47:11 +0000

    clase para debug

---
+++ datos/debug/DebugFacade.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class DebugFacade {
+
+    function DebugFacade() {
+    }
+    
+    static function inicio()
+    {
+    	$GLOBALS['debug_inicio'] = time();
+    	
+    	//print "{$GLOBALS['debug_inicio']}<br>";
+    }
+    
+    static function fin($print=true)
+    {
+    	$fin = time();
+    	$tiempo = ($fin - $GLOBALS['debug_inicio']);
+    	if($print)
+    		print "Inicio: {$GLOBALS['debug_inicio']}<br>Fin: $fin<br>Tiempo Transcurrido: $tiempo<br>";
+    	return $tiempo;
+    }
+}
