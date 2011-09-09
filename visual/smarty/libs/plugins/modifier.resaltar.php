62f40094252ec85b222a000232df2f7e84c6776b
martinezdiaz <martinezdiaz>
2011-09-09 21:32:35 +0000

    modificador para resaltar straings en un string

---
+++ visual/smarty/libs/plugins/modifier.resaltar.php
@@ -0,0 +1,28 @@
+<?php 
+function smarty_modifier_resaltar($str,$filtro)
+{	
+	$res = $str;
+	//var_dump($filtro);
+	if(isset($filtro) && trim($filtro)!="")
+	{	
+		$filtros = explode(" ",$filtro);						
+		foreach($filtros as $f)
+		{	
+			$offset=0;
+			$lengthf = strlen($f);
+			$lengthStr = strlen($str);
+			//var_dump($str);
+			while(($pos = stripos($str,$f,$offset))!==FALSE && $offset<strlen($str))
+			{	
+				//var_dump($pos);
+				$res = substr($str,0,$pos).chr(254).
+				substr($str,$pos,$lengthf).chr(255).substr($str,$pos+$lengthf);
+				$str = $res;
+				$offset = $pos + $lengthf + 2 ;
+			}
+		}
+		$res = str_replace(chr(254),"<span style='background-color:#FFFFBF;'>",$res);
+		$res = str_replace(chr(255),"</span>",$res);
+	}
+	return $res;
+}
