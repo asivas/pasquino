e880412ae6263abb7da4888fd92dae62a30cc776
vidaguren <vidaguren>
2012-10-16 03:55:35 +0000

    clase base de administración (ABMs) junto con la capa de javascript necesaria para poder usarla por jquery-ajax

---
+++ SistemaFCE/js/admin.js
@@ -0,0 +1,220 @@
+function getAccionUrl(modulo,accion,mostrar) {
+	if(mostrar==null) mostrar='full';
+	
+	return "?mod="+modulo+"&accion="+accion+"&display="+mostrar;
+}
+
+function getUrlBaja(mod,id){
+	return getAccionUrl(mod,'baja','plain')+'&id='+id;
+}
+
+function getUrlModif(mod,id){
+	return getAccionUrl(mod,'modif','plain')+'&id='+id;
+}
+
+function getUrlAlta(mod){
+	return getAccionUrl(mod,'alta','plain');
+}
+
+/**
+ * efectúa los binds de los botones de alta, modificación y filtro
+ */
+function setupButtons(selectorBotonera,selectorBotones){
+	
+	if(selectorBotones==null)
+		selectorBotones='.botones';
+	if(selectorBotonera==null)
+		selectorBotonera='td.botones';
+	
+	$(selectorBotonera).find('div').addClass('ui-state-default ui-corner-all').css('float','left').css('margin-left','2px');
+	
+	$(selectorBotones).delegate('a>.ui-icon-pencil','click',function(e){
+		e.preventDefault();
+		if(typeof $fnBindModifBtn == 'function') {
+			$fnBindModifBtn($(this).parent("a").attr("href"));			
+		}		
+	});
+	if(typeof $fnBindAltaBtn == 'function')	$fnBindAltaBtn();
+	if(typeof $fnBindFiltro == 'function')	$fnBindFiltro();
+	
+}
+
+var htmlCargando = "cargando...";
+jQuery.fn.exists = function(){return this.length>0;};
+jQuery.fn.crearDiv = function(idDiv){
+	if(!$("#"+idDiv).exists())
+		return this.append("<div id='"+idDiv+"'></div>");
+	return $("#"+idDiv);
+};
+
+
+var ultimoKeyup=null;
+var filtroAnterior='';
+
+/**
+ * Metodo para realiar el filtro automatico, enviando el submit del form
+ * @param tiempo
+ * @param aInputName
+ * @param aFormID Id del form
+ */
+jQuery['doFilter']=function(tiempo,aInputName){	
+	if(tiempo==ultimoKeyup){
+		var aInput=$("input[name="+aInputName+"]");
+		var aInputVal=aInput.val();
+		if(filtroAnterior!=aInputVal){	
+			filtroAnterior=aInputVal;
+			aInput.parent("form").submit();
+		}
+	}
+};
+
+/**
+ * Metodo agregado a los objetos jQuery para realizar bind de input de filtro, 
+ * con una espera para realizar filtro automatico 
+ * @param aSourceID Objeto desde el cual se carga TargetID
+ * @param aMod Modulo a ejecutar
+ * @param aAction Metodo a Ejecutar
+ * @param aOptions objeto con opciones (success).
+ */
+jQuery.fn.keyUpFilter = function(aSourceID,aMod,aAction,aOptions){
+	var form = $(this).parent("form");
+	form.unbind('submit');
+	form.submit(function(e) {		
+		$("#"+aSourceID).parent().load(getAccionUrl(aMod,aAction,"plain")+ "&" +$(this).serialize() + " #"+aSourceID,
+				function(response, status, xhr) {
+			  		if (status == "error") {
+			  			alert("Ocurrio un Error: " + xhr.status + " " + xhr.statusText);
+			  		}else{
+			  			//setupButtons();
+			  			if(aOptions.success)
+			  				aOptions.success();
+			  		}
+		});
+		e.preventDefault();
+	});
+	$(this).unbind('keyup');
+	$(this).keyup(function(event){
+		setTimeout("$.doFilter("+event.timeStamp+",'"+ $(this).attr('name') +"')",800);
+		ultimoKeyup = event.timeStamp;
+	});
+};
+
+
+/**
+ * Metodo para eliminar una entidad usando ajax procesando la respuesta <status>
+ * @param idMetodoPago
+ * @returns {Boolean}
+ */
+function eliminar(id,mod){
+	var ret=false;
+	$.ajax({
+		url: getAccionUrl(mod,'baja','plain')+"&id="+id,
+		type: 'POST',
+		data: '' , 
+		async:false,
+		success: function(data,  textStatus, jqXHR) {
+			$("#wrapper").crearDiv('tmp');
+			$("#tmp").hide().html(data);
+			if($("#tmp status").attr('status')!='OK'){
+				alert($("#tmp status").attr('msg'));
+				ret=false;
+			}else{
+				alert($("#tmp status").attr('msg'));
+				if($('#idFiltro').exists())
+					$('#idFiltro').submit();
+				ret=true;
+			}
+		}
+	});
+	return ret;
+}
+
+jQuery['dialogoGuardar'] = function(idDialogo,url,titulo,idForm,opciones){
+	var nombreBotonGuardar,valueBotonGuardar,anchoDialogo,callback,nombreBotonCancelar;
+	if(opciones!=null) {
+		nombreBotonGuardar = opciones.nombreBotonGuardar;
+		valueBotonGuardar = opciones.valueBotonGuardar;
+		anchoDialogo = opciones.anchoDialogo;
+		nombreBotonCancelar = opciones.nombreBotonCancelar;
+	}
+	else
+		opciones={};
+
+	if(nombreBotonGuardar==null)	nombreBotonGuardar = 'guardar';
+	if(valueBotonGuardar==null)		valueBotonGuardar = 'Guardar';
+	if(anchoDialogo==null)			anchoDialogo = 510;
+	if(nombreBotonCancelar==null)	nombreBotonCancelar = 'cancelar';
+	
+	if(opciones.modal==null) opciones.modal=true;
+	if(opciones.top==null) opciones.top=69;
+	if(opciones.width==null) opciones.width=anchoDialogo;
+	
+	var dlgOpts = opciones;
+	dlgOpts.title = titulo;
+	
+	$("#wrapper").crearDiv(idDialogo);
+	var dlg = $("#"+idDialogo);
+	dlg.html(htmlCargando).load(url,function(){
+		if( opciones.onLoad )
+			opciones.onLoad();
+		var btn = $("form#"+idForm+" input[name='"+nombreBotonGuardar+"']");
+		var $procesarForm = function(e){
+			$.post('./',$("form#"+idForm).serialize(),
+					function(data) {
+					$("#wrapper").crearDiv("tmp");
+					
+					$("#tmp").hide().html("").html(data);
+					var status = $("#tmp status").attr('status'); 
+					dlg.attr('status',status);
+					if(status=='OK')
+					{	
+						if( opciones.success )
+							opciones.success();
+						dlg.dialog('close').remove();
+					}
+					else if(status=='ERR')
+						alert($("#tmp status").attr('msg'));
+				}
+			);
+			e.preventDefault();
+		};
+		
+		if(btn.exists())
+			btn.button($procesarForm).click($procesarForm).attr('value',valueBotonGuardar);
+		else
+			$("form#"+idForm).submit($procesarForm);
+		
+		var btnCancelar = $("form#"+idForm+" input[name='"+nombreBotonCancelar+"']");
+		if(btnCancelar.exists())
+			btnCancelar.button().click(function(e){dlg.html("").dialog('close');});
+		
+	}).dialog(dlgOpts);
+};
+
+function initModulo(idDialogo,nombreEntidadPrincipal,idForm,nombreCampoFiltro,idBotonAlta,idLista,aMod) {
+	var $filtroFormSubmit = function(){$("input[name='"+nombreCampoFiltro+"']").parent("form").submit();};
+	$fnBindAltaBtn = function(){ 
+		$("#"+idBotonAlta).botonAlta(idDialogo,"Nuevo "+nombreEntidadPrincipal ,idForm,{success:$filtroFormSubmit});
+	};
+	$fnBindModifBtn = function(href){ 
+		$.dialogoGuardar(idDialogo,href+"&display=plain","Modificar "+nombreEntidadPrincipal,idForm,{success:$filtroFormSubmit});
+	};
+	$fnBindFiltro = function() {
+		$("input[name='"+nombreCampoFiltro+"']").keyUpFilter(idLista,aMod,"listar");
+	};
+	setupButtons();
+}
+
+/**
+ * Crea una funcion de jQuery que le asigna la funcionalidad de boton de alta al elemento que la llama
+ * suponiendo que es un contenedor html (div,td,span...) que tiene un a href con la URL para el alta
+ */
+jQuery.fn.botonAlta = function(idDialogo,tituloDialogo,idFormAlta,opciones) {
+	var linkAlta = $(this).addClass("ui-state-default ui-corner-all").css('float','right').css('display','block').find('a');
+	if(!linkAlta.find("span.ui-icon-plus").exists())
+		linkAlta.append('<span class="ui-icon ui-icon-plus"></span>');
+	$(this).delegate("a","click",function(e){
+		e.preventDefault();
+		$.dialogoGuardar(idDialogo,$(this).attr('href')+"&display=plain",tituloDialogo,idFormAlta,opciones);
+	});
+};
---
+++ SistemaFCE/modulo/BaseAdminMod.class.php
@@ -0,0 +1,233 @@
+<?php
+// si no se definió en otro lado cargo el smarty 3, para que no se cargue el 2 en baseMod
+if(!class_exists('Smarty'))
+	require_once('visual/smarty3/libs/Smarty.class.php');
+
+require_once 'modulo/BaseMod.class.php';
+
+
+/**
+ * Modulo Esqueleto de administración
+ * 
+ * Tiene los tpls de lista, form y un dao principal. Tambien se definen los 
+ * metodos de alta, baja y modificación asi como el que genera el form y tiene un guardar 
+ * que devuelve por ajax un mensaje de success  
+ * @author lucas
+ *
+ */
+abstract class BaseAdminMod extends BaseMod {
+	protected $mainDao;
+	protected $_tplLista;
+	protected $_tplForm;
+	
+	function __construct($form, $dao, $sessionHandler=null, $listaTplPath=null, $formTplPath=null, $skinDirname=null, $tilePathName = 'Admin')
+	{
+		if(isset($sessionHandler))	$this->session = $sessionHandler;
+			
+		parent::__construct($skinDirname,false); // como se usa mayormente jquery se pasa por defecto el conXajax en false
+
+		$this->_form = $form;
+		$this->mainDao = $dao;
+		
+		$this->_tplLista = $listaTplPath;
+		$this->_tplForm = $listaTplPath;
+		
+		$tConf = Configuracion::getTemplateConfigByDir($templateDir); 
+		$this->_tilePath = Configuracion::findTplPath($tConf,$tilePathName);
+		
+		//busco el dir esperado por defecto de los tpls del modulo
+		$dir = strtolower( str_replace("Mod", "", get_class($this)) );
+		//si no está seteado el tpl lista cargo un path por defecto con el dir y lista.tpl
+		if(!isset($this->_tplLista)) $this->_tplLista = "{$dir}/lista.tpl";
+		//si no está seteado el tpl form cargo un path por defecto con el dir y form.tpl	
+		if(!isset($this->_tplForm)) $this->_tplForm = "{$dir}/form.tpl";
+		
+		if(isset($nada)) //TODO: borrar esta linea y la siguiente, está para que autocomplete
+			$this->mainDao = new DaoBase();
+	} 
+	
+	/**
+	 * Envia (haciendo display) un mensaje de status usando el tpl de msgStatus
+	 * @param string $status
+	 * @param string $mensaje
+	 * @param array $otros
+	 */
+	private function mensaje($status,$mensaje,$otros=null)
+	{
+		$this->smarty->assign("status",$status);
+		$this->smarty->assign("msg",$mensaje);
+		$this->smarty->assign("otros",$otros);		
+		$this->smarty->display('string:<status msg="{$msg}" status="{$status}" {foreach from=$otros key=k item=valor} {$k}={$valor} {/foreach}></status>');
+		die();
+	}
+	
+	/**
+	 * Envia un mensaje de OK en xml usando mensaje
+	 * @param string $mensaje el mensaje
+	 * @param array $otros arreglo de otros atributos para agregarle al <status>
+	 */
+	protected function mensajeOK($mensaje,$otros=null)
+	{
+		$this->mensaje("OK", $mensaje, $otros);
+	}
+	
+	/**
+	 * 
+	 * Envia un mensaje de ERR (error) via xml usando mensaje
+	 * @param string $mensaje el mensaje
+	 * @param array $otros arreglo de otros atributos
+	 */
+	protected function mensajeERR($mensaje,$otros=null)
+	{
+		$this->mensaje("ERR", $mensaje, $otros);
+	}
+	
+	/**
+	 * Obtiene el id del item que se desea buscar/generar a partir del req 
+	 * @param array $req
+	 */
+	protected function getItemId($req) {
+		$id = $this->mainDao->getIdElementoDeArreglo($req);
+		return $id;
+	}
+	
+	
+	/**
+	 * Valida si el arreglo req tiene las variables completas necesrias como para definir un id 
+	 * del objeto de ABM
+	 * @param array $req
+	 */
+	protected function validateId($req){
+		return isset($req['id']);
+	}
+	
+	
+	/* (non-PHPdoc)
+	 * 
+	 * @see BaseMod::getFiltro()
+	 */
+	function getFiltro($req) {
+		$c = parent::getFiltro($req);
+		if(isset($req['filtroNombre'])){
+			$this->smarty->assign('filtroNombre',$req['filtroNombre']);
+			$c->add(Restricciones::like('nombre',"%{$req['filtroNombre']}%"));
+		}
+		return $c;
+	}
+	
+	/**
+	 * Guarda un objeto usando su dao (el $this->mainDao)
+	 * @param object $aObj
+	 */
+	protected function guardar($aObj) {
+		if(method_exists($aObj, "toString"))
+			$strInfo = $aObj->toString();
+		else 
+			$strInfo = "El elemento de tipo ".get_class($aObj); 
+		if($this->mainDao->save($aObj))
+			$this->mensajeOK("{$strInfo} fue guardado con exito",array('id'=>$aObj->getId()));
+		else 
+			$this->mensajeERR("{$strInfo} no se pudo guardar ". $this->mainDao->getLastError());
+	}
+	
+	/**
+	 * Envía un mensaje de errror ante fallo de validaci�n del formulario
+	 */
+	protected function sendValidateErrorMsg() {
+		$strErr = "No se cumplen las reglas de validación:";
+		$f = $this->renderForm();
+		foreach($f['errors'] as $nombreRegla=>$err)
+			$strErr .= "\n".$err;			
+		$this->mensajeERR($strErr);
+	}
+	
+	/**
+	 * 
+	 * En este metodo se pueden agregar (probablemente desde el req) las opciones extra que no
+	 * vienen de mapeo antes de guardar
+	 * @param array $req
+	 * @param mixed $aObj
+	 */
+	protected function addExtraProps($req,&$aObj){}
+	
+	/* (non-PHPdoc)
+	 * @see BaseMod::alta()
+	 */
+	public function alta($req) {
+			
+		if($this->getForm()->validate())
+		{
+			$aObj = $this->mainDao->crearDesdeArreglo($req);			
+			$this->addExtraProps($req, $aObj);
+			$this->guardar($aObj);
+		}
+		else
+			$this->sendValidateErrorMsg();
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseMod::modificacion()
+	 */
+	public function modificacion($req) {
+		if($this->getForm()->validate())
+		{
+			$aObj = $this->mainDao->crearDesdeArreglo($req);			
+			// en caso de que el id no sea simple o no est� 
+			// pasado con el mismo nombre de la propiedad
+			$aObj->setId($this->getItemId($req));
+			$this->addExtraProps($req, $aObj);
+			$this->guardar($aObj);
+		}
+		else
+			$this->sendValidateErrorMsg();
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseMod::baja()
+	 */
+	public function baja($req){
+		if(method_exists($aObj, "toString"))
+			$strInfo = $aObj->toString();
+		else 
+			$strInfo = "el elemento de tipo ".get_class($aObj); 
+		
+		$id = $this->getItemId($req);
+			
+		if ($this->mainDao->deletePorId($id))
+			$this->mensajeOK("Se pudo eliminar {$strInfo} #{$id} ");
+		else 
+			$this->mensajeERR("No se puede eliminar {$strInfo} [Error {$this->mainDao->getLastError()}]");
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseMod::lista()
+	 */
+	public function lista($req){
+		$aObjs = $this->mainDao->findBy($this->getFiltro($req),$req['sort']);
+		
+		$nombreClase="";
+		if(count($aObjs)>0)
+		{
+			$aObj = current($aObjs);
+			$nombreClase = get_class($aObj);
+		} 
+		$this->smarty->assign('lista'.$nombreClase,$aObjs);
+		$this->mostrar($this->_tplLista,$req['display']);
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseMod::form()
+	 */
+	public function form($req){
+		$id = $this->getItemId($req);
+		if($this->validateId($req))
+		{	
+			$a = $this->mainDao->findById($id);
+			$this->smarty->assign(get_class($a),$a);
+			$this->getForm()->setDefaults($a);
+		}
+		$this->renderForm();
+		$this->mostrar($this->_tplForm,$req['display']);		
+	}
+	
+}
