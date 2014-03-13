56019dbd4ab1cf9fdd0447ff35392ac5bcedafec
vidaguren <vidaguren>
2014-03-13 14:17:13 +0000

    agregado a standard paths src para compatiblizar con algunas aplicaciones que definían el dir de modulos, daos y entidades como src

--- SistemaFCE/util/Configuracion.class.php
+++ SistemaFCE/util/Configuracion.class.php
@@ -4,81 +4,81 @@ class Configuracion {
     static private $rootDir;
     static private $confDir='conf';
     static private $confFilename='config.xml';
-    
+
     function Configuracion() {
-        
+
     }
-    
+
 	static public function autoload_entidad($nombre) {
         @include_once("entidades/{$nombre}.class.php");
 		@include_once("SistemaFCE/entidad/{$nombre}.class.php");
         @include_once("{$nombre}.class.php");
     }
-    
+
     public static function initSistema($rutaSysRoot=null,$pathsIncludePath=null)
     {
-        
+
         if(Configuracion::getSystemRootDir()==null)
         {
             if(!isset($rutaSysRoot))
             {
-            	$dbt = debug_backtrace();            	
+            	$dbt = debug_backtrace();
             	$rutaSysRoot = dirname(dirname($dbt[0]['file']));
-            } 
+            }
         	Configuracion::setSystemRootDir($rutaSysRoot);
         }
-        
+
         Configuracion::setIncludePath($pathsIncludePath);
-		
+
         spl_autoload_register('Configuracion::autoload_entidad');
         //{{{ Incluir modulos
-        Configuracion::incluirModulos();    
+        Configuracion::incluirModulos();
         ///}}}
-        
+
         if(strpos(strtoupper($_SERVER['SERVER_SIGNATURE']),"WIN")!==FALSE) /*servidor Windows*/
-            setlocale (LC_TIME, "spanish"); 
+            setlocale (LC_TIME, "spanish");
         else //supongo unix //if(strpos(strtoupper($_SERVER['SERVER_SIGNATURE']),"UNIX")!==FALSE)/*sevidor unix*/
             setlocale (LC_TIME, "es_AR", "es_AR.UTF-8");
-    } 
-    
+    }
+
     public static function getDefaultMod()
     {
     	$config = Configuracion::getConfigXML();
         return $config->modulos['default'];
     }
-    
+
     public static function getTemplateConfigByDir($dir)
     {
     	return Configuracion::getTemplateConfigByNombre($dir);
     }
-    
+
     public static function getTemplateConfigByNombre($nombre_o_dir)
-    {   
+    {
     	$config = Configuracion::getConfigXML();
         $templates = $config->templates;
-        
+
         if(empty($nombre_o_dir))
             $nombre_o_dir = $templates['default'];
-        
+
         foreach($templates->template as $template)
         {
         	$tDir = "{$template['nombre']}";
         	if(empty($tDir)) $tDir = "{$template['dir']}";
-        	
+
             if($tDir==$nombre_o_dir)
-            {   
+            {
                 return $template;
             }
         }
         return null;
     }
-    
+
     public static function getAppName()
     {
     	$config = Configuracion::getConfigXML();
         return $config['nombre'];
     }
-    
+
     public static function getDateFormat()
     {
         $config = Configuracion::getConfigXML();
@@ -96,12 +96,12 @@ class Configuracion {
         $config = Configuracion::getConfigXML();
 		return "{$config->{'date-formats'}->{'time-format'}}";
     }
-    
+
     public static function getDefaultTemplateConfig()
     {
         return Configuracion::getTemplateConfigByDir("");
     }
-    
+
     public static function getDefaultDataSource()
     {
         $config = Configuracion::getConfigXML();
@@ -111,10 +111,10 @@ class Configuracion {
 
         if(!$dataSources)
             $dataSources = $config->{"data-sources"};
-        
+
         return (string)$dataSources['default'];
     }
-    
+
     private static function getDBAttribute($attribName,$nombreDataSource)
     {
         $config = Configuracion::getConfigXML();
@@ -124,7 +124,7 @@ class Configuracion {
 
         if(!$dataSources)
             $dataSources = $config->{"data-sources"};
-            
+
         if(!isset($nombreDataSource))
             $nombreDataSource = Configuracion::getDefaultDataSource();
         foreach($dataSources->{"data-source"} as $ds)
@@ -132,64 +132,64 @@ class Configuracion {
             if($ds['name'] == $nombreDataSource)
                 return $ds[$attribName];
         }
-        
+
         return "";
     }
-    
+
     public static function getDBMS($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("dbms",$nombreDataSource); 
+        return Configuracion::getDBAttribute("dbms",$nombreDataSource);
     }
-    
+
     public static function getDbDSN($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("DSN",$nombreDataSource); 
+        return Configuracion::getDBAttribute("DSN",$nombreDataSource);
     }
-    
+
     public static function getDbHost($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("host",$nombreDataSource); 
+        return Configuracion::getDBAttribute("host",$nombreDataSource);
     }
-    
+
     public static function getDbName($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("db-name",$nombreDataSource); 
+        return Configuracion::getDBAttribute("db-name",$nombreDataSource);
     }
-    
+
     public static function getDbUser($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("username",$nombreDataSource); 
+        return Configuracion::getDBAttribute("username",$nombreDataSource);
     }
-    
+
     public static function getDbPassword($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("password",$nombreDataSource); 
+        return Configuracion::getDBAttribute("password",$nombreDataSource);
     }
-    
+
     public static function getDbPort($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("port",$nombreDataSource); 
+        return Configuracion::getDBAttribute("port",$nombreDataSource);
     }
-    
+
     public static function getVersion()
     {
     	$config = Configuracion::getConfigXML();
         return $config['version'];
     }
-    
+
     public static function setSystemRootDir($rootDir)
     {
     	//$GLOBALS['ROOT_DIR'] = $rootDir;
     	self::$rootDir = $rootDir;
     }
-    
+
     public static function getSystemRootDir()
-    {   
+    {
         /*if(isset($GLOBALS['ROOT_DIR'])) return $GLOBALS['ROOT_DIR']; */
     	if(isset(self::$rootDir))
     		return self::$rootDir;
     }
-    
+
     private static function getStandardSubdirs($dir) {
     	return array($dir.DIRECTORY_SEPARATOR.'entidades'.DIRECTORY_SEPARATOR.'daos',
         		$dir.DIRECTORY_SEPARATOR.'utils',
@@ -197,28 +197,28 @@ class Configuracion {
         		$dir.DIRECTORY_SEPARATOR.'auth',
         		$dir.DIRECTORY_SEPARATOR.'rules');
     }
-    
+
     public static function setIncludePath($otrosPaths=null)
     {
     	$pathSep = PATH_SEPARATOR;
-        
+
         $sysRoot = Configuracion::getSystemRootDir();
-        
+
         $inc_path = ini_get("include_path");
-        
+
         if(!isset($otrosPaths) )
         	$otrosPaths= array();
-        	
+
        	//si es string lo hago array
        	if(is_string($otrosPaths))
        		$otrosPaths[] = $otrosPaths;
-        
-        $standardDirs = array('clases', 'application');
-        
+
+        $standardDirs = array('clases', 'src', 'application');
+
         foreach ($standardDirs as $dirname)
         	if(file_exists($sysRoot.DIRECTORY_SEPARATOR.$dirname) && array_search($dirname, $otrosPaths)===FALSE)
         		$otrosPaths[] = $dirname;
-        
+
         if(is_array($otrosPaths))
         {
         	foreach($otrosPaths as $dir)
@@ -243,7 +243,7 @@ class Configuracion {
 
         $inc_path = ini_set("include_path",$inc_path);
     }
-    
+
     public static function incluirModulos()
     {
         $config = Configuracion::getConfigXML();
@@ -257,17 +257,17 @@ class Configuracion {
             if(!empty($mod->archivos->coreDir['ruta']))
                 $inc .= "{$mod->archivos->coreDir['ruta']}/";
             $inc .= $mod->archivos->coreDir->archivoPrincipal['nombre'];
-            
-            require_once($inc);   	
+
+            require_once($inc);
         }
     }
-    
+
     public static function getConfigXML()
     {
     	$config = simplexml_load_file(Configuracion::getSystemRootDir().DIRECTORY_SEPARATOR.self::$confDir.DIRECTORY_SEPARATOR.self::$confFilename);
         return $config;
     }
-    
+
     public static function findTplPath($tConf,$sysName = 'Default')
     {
         //recorro los archivos
@@ -278,71 +278,71 @@ class Configuracion {
             {
                 $sn = $arch['sys-name'];
                 if(isset($sn) && $sn == $sysName)
-                {   
+                {
                     return (string)$arch['nombre'];
                 }
             }
         }
-        
+
         //recorro los dir si los tiene
         if(!empty($tConf->{'dir'}))
         {
             foreach($tConf->{'dir'} as $dir)
-            {   
+            {
             	$path = Configuracion::findTplPath($dir,$sysName);
                 if(!empty($path))
-                {   
+                {
                     return "{$dir['ruta']}/$path";
                 }
             }
-        } 
-        
+        }
+
         return null;
     }
-    
+
     public static function getDefaultTplPath($templateDir="")
     {
     	//$tplPath = "";
-        $tConf = Configuracion::getTemplateConfigByDir($templateDir);        
+        $tConf = Configuracion::getTemplateConfigByDir($templateDir);
         return Configuracion::findTplPath($tConf,'Default');
     }
-    
+
     public static function getBaseTplPath($templateDir="")
     {
         //$tplPath = "";
         $tConf = Configuracion::getTemplateConfigByDir($templateDir);
         return Configuracion::findTplPath($tConf,'Base');
     }
-    
-    
+
+
     public static function agregarModulo()
     {
-    	//TODO: a partir de un XML de modulo importarlo al XML de sistema 
+    	//TODO: a partir de un XML de modulo importarlo al XML de sistema
     }
-    
+
     public static function quitarModulo($nombre)
     {
         //TODO: que a partir del $nombre lo borre del XML de sistema
-        // tambien deber�a borrarl los archivos 
+        // tambien deber�a borrarl los archivos
     }
-    
+
     public static function agregarTemplate()
     {
-        //TODO: a partir de un XML de template importarla al XML de sistema 
+        //TODO: a partir de un XML de template importarla al XML de sistema
     }
-    
+
     public static function quitarTemplate($nombreDir)
     {
         //TODO: que a partir del $nombreDir borre la template del XML de sistema
-        // tambien deber�a borrarl los archivos 
+        // tambien deber�a borrarl los archivos
     }
-    
+
     public static function getModulosConfig()
     {
         $config = Configuracion::getConfigXML();
         return $config->modulos;
     }
-    
+
     public static function getMappingConfigClase($clase)
     {
         $config = Configuracion::getConfigXML();
@@ -356,16 +356,16 @@ class Configuracion {
         }
         return null;
     }
-    
+
     public static function getMappingClase($nombreClase,$xmlMappingFile = null)
-    {    
+    {
         if(empty($xmlMappingFile))
         {
             $archivoMappings = "";
             $config = Configuracion::getConfigXML();
-            
+
             $mappings = $config->mappings;
-            
+
             $map = Configuracion::getMappingConfigClase($nombreClase);
             if(isset($map))
             {
@@ -376,23 +376,23 @@ class Configuracion {
                 }
                 $archivoMappings = "{$mappings['path']}/{$archivo}";
             }
-            
+
             //el archivo obtenido est� puesto relativo a la raiz del proyecto
             $xmlMappingFile = Configuracion::getSystemRootDir()."/{$archivoMappings}";
         }
-        
+
         $map = simplexml_load_file($xmlMappingFile);
-        return $map;	
+        return $map;
     }
-    
-    
+
+
     public static function ejecutarSistema($req=null,$m=null)
     {
         if(!isset($req))
             $req = $_REQUEST;
         /*
          Experimental, para poder usar una URL amigable a buscadores
-         
+
         $ruta = str_replace($_SERVER['SCRIPT_NAME'],'',$_SERVER['REQUEST_URI']);
 
         $ruta = str_replace("?{$_SERVER['QUERY_STRING']}",'',$ruta);
@@ -401,9 +401,9 @@ class Configuracion {
         {
             $modAcc = split("/",$ruta);
             $m = $modAcc[1];
-            $req['accion'] = $modAcc[2]; 
+            $req['accion'] = $modAcc[2];
         }
-        
+
         */
         if(isset($req['mIt']))
         {
@@ -412,55 +412,55 @@ class Configuracion {
         		if(isset($menuItemConf['alias']))
         			if( ($aliasItemConf = Configuracion::getMenuItemConfByName($menuItemConf['alias']))!=null)
         				$menuItemConf = $aliasItemConf;
-        		
+
        			$m = (string)$menuItemConf['mod'];
        			$req['accion'] = (string)$menuItemConf['accion'];
         	}
         }
-        
+
         if(!isset($m))
         {
             $m = $req['mod'];
             if(!empty($_GET['mod'])) $m = $_GET['mod'];
             if(!empty($_POST['mod'])) $m = $_POST['mod'];
         }
-        
+
         $modName = ucfirst($m)."Mod";
-       
+
         if(!class_exists($modName))
         {
             $modName = Configuracion::getDefaultMod().'Mod';
             if(!isset($req['accion'])) //solo actualizo si se llama sin mod y sin acción
             	Configuracion::installOrUpdateDatabase();
-        } 
-        
+        }
+
         $mod = new $modName();
-	        
-	    $mod->ejecutar($req);	
-        
+
+	    $mod->ejecutar($req);
+
     }
-    
+
     /**
      * Obtiene la configuraci�n del modulo cuyo nombre es dado
-     * 
+     *
      * @param string $mod nombre del modulo
      */
     public static function getConfigModulo($nombreMod)
     {
     	if(strpos($nombreMod,'Mod')===FALSE)
           $nombreMod .= 'Mod';
-        
+
         $modulos = Configuracion::getModulosConfig();
         foreach($modulos->modulo as $mod)
-        {	
+        {
             if("{$mod['nombre']}Mod" == $nombreMod)
                 return $mod;
         }
         return null;
     }
-    
+
     /**
-     * 
+     *
      * Obtiene el nombre de la accion predeterminada del modulo dado
      * @param string $mod nombre del modulo
      * @return string el nombre de la accion predeterminada
@@ -470,23 +470,23 @@ class Configuracion {
     	$modConfig = Configuracion::getConfigModulo($mod);
     	$accion = (string)$modConfig->acciones['default'];
     	if(empty($accion)) $accion = 'listar';
-    	
+
     	return $accion;
     }
-    
-    
+
+
     public static function getEntidadUsuarioClass() {
     	$config = Configuracion::getConfigXML();
     	$mappings = $config->mappings;
-    	
+
     	if(isset($mappings['entidadUsuario']))
     		return (string)$mappings['entidadUsuario'];
-    	
+
     	return "Usuario";
     }
-    
+
     /**
-     * 
+     *
      * Obtiene desde XML de configuración el nombre de clase del objeto log
      */
     //FIXME: this method should return array of loggers configurations
@@ -496,10 +496,10 @@ class Configuracion {
     	if (isset($config->loggers))
     		foreach ($config->loggers as $logger) {
     			return (String)$logger->logger['class'];
-    		}    		
+    		}
     	return null;
     }
-    
+
     /**
      * Obtiene la configuración de un item submenú de menu de la configuración a partir del nombre
      * o null si no la encuentra
@@ -518,19 +518,19 @@ class Configuracion {
     	}
     	return null;
     }
-    
+
     /**
      * Obtiene la configuración de un item del menú a partir del nombre
      * o null si no la encuentra
      * @param $name nombre del menuItem del que se quiere obtener la config
      */
-    public static function getMenuItemConfByName($name) 
+    public static function getMenuItemConfByName($name)
     {
 	    $modulosConfig = Configuracion::getModulosConfig();
 	    foreach($modulosConfig->modulo as $modConf)
 	    {
 	    	$nam = (string)$modConf->menuPrincipal['name'];
-	    	
+
 	    	if($nam == $name)
 	    		$menuItemConf = $modConf->menuPrincipal;
 	    	else
@@ -540,24 +540,24 @@ class Configuracion {
 	    	{
 		    	if(empty($menuItemConf['mod']))
 		    		$menuItemConf['mod'] = (string)$modConf['nombre'];
-		    	
+
 		    	return $menuItemConf;
 	    	}
 	    }
 	    return null;
     }
-    
+
     public static function getDBUpdaterClass() {
     	$config = Configuracion::getConfigXML();
-    	$className = (string)$config['dbUpdater-class'];  
+    	$className = (string)$config['dbUpdater-class'];
    		return $className;
     }
-    
+
     public static function getPropertiesManagerClass(){
     	$config = Configuracion::getConfigXML();
-    	return (string)$config['properties-manager-class'];    		
+    	return (string)$config['properties-manager-class'];
     }
-    
+
     public static function getVersionDB() {
     	$propsMgrClass = Configuracion::getPropertiesManagerClass();
     	if(!empty($propsMgrClass))
@@ -568,7 +568,7 @@ class Configuracion {
     	}
     	return "0";
     }
-    
+
     static private function installOrUpdateDatabase() {
     	$dbUpdClass = Configuracion::getDBUpdaterClass();
     	if(empty($dbUpdClass)) return;
@@ -576,11 +576,11 @@ class Configuracion {
     	$versionEsperada = $dbUpdClass::$expectedDBVersion;
     	if($versionEsperada<=$versionActual)
     		return;
-    	
+
     	$dbUpd = new $dbUpdClass();
     	$dbUpd->updateDb($versionActual);
     }
-    
+
 }
 
 class_alias('Configuracion','SistemaFCE');
