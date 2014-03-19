1017fce47e954fcd707ea10ac51df80990c7b4f8
vidaguren <vidaguren>
2014-03-19 23:47:39 +0000

    lo de la coneccion reciclada no funcionó ni un poco

--- SistemaFCE/dao/DaoBase.class.php
+++ SistemaFCE/dao/DaoBase.class.php
@@ -89,15 +89,15 @@ abstract class DaoBase {
     {
         if(!isset($dataSource))
             $dataSource = Configuracion::getDefaultDataSource();
-		if(!isset(self::$dbConections[$dataSource]))
-		{
+// 		if(!isset(self::$dbConections[$dataSource]))
+// 		{
 	        self::$dbConections[$dataSource] = &ADONewConnection(Configuracion::getDBMS($dataSource)); # eg 'mysql' or 'postgres'
 	        self::$dbConections[$dataSource]->SetFetchMode(ADODB_FETCH_ASSOC);
 	        if(Configuracion::getDbDSN($dataSource)!='')
 	        	self::$dbConections[$dataSource]->NConnect(Configuracion::getDbDSN($dataSource), Configuracion::getDbUser($dataSource), Configuracion::getDbPassword($dataSource));
 	        else
 	        	self::$dbConections[$dataSource]->NConnect(Configuracion::getDbHost($dataSource), Configuracion::getDbUser($dataSource), Configuracion::getDbPassword($dataSource), Configuracion::getDbName($dataSource));
-		}
+// 		}
         return self::$dbConections[$dataSource];
     }
 
