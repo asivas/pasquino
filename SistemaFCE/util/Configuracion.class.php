fc6e6f4f9c3082cd3874c9d6c8fc6bcbe0b3f29a
vidaguren <vidaguren>
2015-03-03 19:00:07 +0000

    Cambio de estrategia de puerto diferente (arma el dsn)

--- SistemaFCE/util/Configuracion.class.php
+++ SistemaFCE/util/Configuracion.class.php
@@ -143,14 +143,6 @@ class Configuracion {
         return "";
     }
 
-    public static function getDbHostPort($nombreDataSource = null)
-    {
-    	$host = Configuracion::getDbHost($nombreDataSource);
-    	$port = Configuracion::getDbPort($nombreDataSource);
-    	if($port != '')	$host .= ":{$port}";
-    	return $host;
-    }
-
     public static function getDBMS($nombreDataSource = null)
     {
         return Configuracion::getDBAttribute("dbms",$nombreDataSource);
@@ -158,7 +150,16 @@ class Configuracion {
 
     public static function getDbDSN($nombreDataSource = null)
     {
-        return Configuracion::getDBAttribute("DSN",$nombreDataSource);
+        $dsn = Configuracion::getDBAttribute("DSN",$nombreDataSource);
+        if(empty($dsn))
+        {
+        	$port = Configuracion::getDbPort($nombreDataSource);
+        	if(!empty($port))
+        	{
+        		$dsn = Configuracion::getDBMS($nombreDataSource)."://".Configuracion::getDbUser().":".Configuracion::getDbPassword()."@".Configuracion::getDbHost()."/".Configuracion::getDbName()."?port=".Configuracion::getDbPort();
+        	}
+        }
+    	return $dsn;
     }
 
     public static function getDbHost($nombreDataSource = null)
