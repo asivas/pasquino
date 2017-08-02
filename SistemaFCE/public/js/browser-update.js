be69a0f35a36f12f6421c8f1a1277a1e77d71a3c
Lucas <lucas.vidgauren@asivas.com.ar>
2017-08-02 14:32:33 -0300

    Browser update vía https

--- SistemaFCE/public/js/browser-update.js
+++ SistemaFCE/public/js/browser-update.js
@@ -7,6 +7,6 @@ window.onload=function(){
  try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
  var e = document.createElement("script"); 
  e.setAttribute("type", "text/javascript"); 
- e.setAttribute("src", "http://browser-update.org/update.js"); 
+ e.setAttribute("src", "https://browser-update.org/update.js");
  document.body.appendChild(e); 
 } 
