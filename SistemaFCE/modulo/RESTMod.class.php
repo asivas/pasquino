443e7224eb2b0d2773e638ede50f1741b0d7f1b8
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2020-02-12 17:44:21 -0300

    Agregado accessControlalowed headers

--- SistemaFCE/modulo/RESTMod.class.php
+++ SistemaFCE/modulo/RESTMod.class.php
@@ -416,6 +416,7 @@ class RESTMod {
             header("Connection: keep-alive");
             header("Access-Control-Allow-Origin: *");
             header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, DELETE");
+            header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
             exit;
         }
 		
