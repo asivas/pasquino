dd36fd76a8ea9873eac16c6302d2d3dcac9f8f50
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2021-03-24 20:44:42 -0300

    pQn conf file name used in a2enconf 2

--- install.sh
+++ install.sh
@@ -82,11 +82,11 @@ read -p "Do you wish to install the alias with a2enconf [Y/n]?" yn
 case $yn in	    
     [Nn]* ) break;;
     [Yy]* )
-        pQnConfName="${$pQnConfFileName%.*}";
+        pQnConfName="${pQnConfFileName%.*}";
 	    a2enconf $pQnConfName;
 	 ;;
     * ) 
-	   pQnConfName="${$pQnConfFileName%.*}";
+	   pQnConfName="${pQnConfFileNamee%.*}";
 	   a2enconf $pQnConfName;
 	 ;;
 esac
